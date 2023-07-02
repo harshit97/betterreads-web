d5a351c1f5cf433317f4f73743ff25f571db3a85
Harshit Agarwal <harshit.agarwal@uber.com>
2023-07-02 13:45:32 +0530

    adding comment for testing

--- src/test/java/io/harshit/betterreadsweb/BetterreadsWebApplicationTests.java
+++ src/test/java/io/harshit/betterreadsweb/BetterreadsWebApplicationTests.java
@@ -3,6 +3,7 @@ package io.harshit.betterreadsweb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+# comment for testing
 @SpringBootTest
 class BetterreadsWebApplicationTests {
 
